f93c4345ea60b5767a58ef4d42c79d9a2a7480fc
Jonas Troyer <troyerjl2011@gmail.com>
2019-11-22 04:03:41 -0800

    Not working all the way but whatever

--- nested.py
+++ nested.py
@@ -3,12 +3,31 @@
 """
 Module docstring: One line description of what your program does.
 """
-__author__ = "Your Github Username"
+__author__ = "troyerjl and geeks for geeks"
 
 import sys
 
+
+
 def main(args):
-    """Add your code here"""
+    with open("input.txt", "r") as f:
+        list_of_strings = f.readlines()
+    close_brackets = [ ")", "]", "}", ">", "*)" ]
+    open_brackets = [ "(", "[", "{", "<", "(*)" ] 
+    for string in list_of_strings:
+        stack = []
+        for i in string:
+            if i in open_brackets:
+                stack.append(i)
+            elif i in close_brackets:
+                pos = close_brackets.index(i) 
+                if ((len(stack) > 0) and 
+                (open_brackets[pos] == stack[len(stack)-1])): 
+                    stack.pop()
+                else:
+                    print("Nos")
+        if len(stack) == 0:
+            print("Yes")
     
 
 if __name__ == '__main__':
